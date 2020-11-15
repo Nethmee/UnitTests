96737752177408b022ca1a08ea6997efb816f0a3
Nethmee <nssellahewa@gmail.com>
2020-11-15 18:00:19 +0530

    added custom tests for app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,7 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {isTsAnyKeyword } from '@babel/types';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -6,3 +9,8 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+it("App renders without crashing",()=>{
+  const div = document.createElement('div');
+  ReactDOM.render(<App></App>,div)
+})
